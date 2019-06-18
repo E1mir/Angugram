7bf58ea86f35e4a1ea28b1d51ece14595e7426d2
Elmir Ismayilzade <kryternext@smartup.school>
2019-06-18 23:43:45 +0400

    Update paths in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { MaterialDesignModule } from './material-design/material-design.module';
-import { MaterialTestComponent } from './material-test/material-test.component';
+import { AppComponent } from '@app/app.component';
+import { MaterialDesignModule } from '@app/material-design/material-design.module';
+import { MaterialTestComponent } from '@app/material-test/material-test.component';
 
 @NgModule({
   declarations: [
