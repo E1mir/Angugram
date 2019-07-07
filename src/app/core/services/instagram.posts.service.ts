083df858be5fd5b4bf861bc321d0b4fdd71c0b35
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-08 01:11:52 +0400

    Create instagram post service with search by tag method

---
+++ src/app/core/services/instagram.posts.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class InstagramPostsService {
+  private MAIN_URL = 'https://instagram.com';
+
+  constructor(private http: HttpClient) {
+  }
+
+  searchByTag(tag: string) {
+    return this.http.get(`${this.MAIN_URL}/explore/tags/${tag}/`);
+  }
+}
