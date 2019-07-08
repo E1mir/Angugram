544b9150d54392b4d61b1d1a43442383df1cad3a
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-08 19:06:42 +0400

    Rename folder from header to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from '@app/app.component';
 import { MaterialDesignModule } from '@app/material-design/material-design.module';
 import { MaterialTestComponent } from '@app/material-test/material-test.component';
-import { NavbarComponent } from './header/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialGridTestComponent } from './material-grid-test/material-grid-test.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { InstagramInterceptorService } from '@app/core/interceptors/instagram-interceptor.service';
