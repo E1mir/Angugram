e27abac46d87d775c0d8329100a650f1072c4efa
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-08 19:05:36 +0400

    Create layout of navigation bar

--- src/app/header/navbar.component.ts
+++ src/app/header/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InstagramService } from '@app/core/services/instagram.service';
+import { User } from '@dt/interfaces/instagram';
+import { MY_PROFILE_USERNAME } from '@app/core/variables/constants';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  me: User = null;
+
+  constructor(private instagramService: InstagramService) {
+  }
 
   ngOnInit() {
+    this.instagramService.getAccountByUsername(MY_PROFILE_USERNAME).subscribe(
+      (user) => {
+        this.me = user;
+      }
+    );
   }
 
 }
