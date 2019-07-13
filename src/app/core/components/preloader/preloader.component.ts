308ff8afdcbb1f5f4ef2970a38d9ec8edb028ae5
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-13 10:45:26 +0400

    Remove unused field

--- src/app/core/components/preloader/preloader.component.ts
+++ src/app/core/components/preloader/preloader.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preloader.component.scss']
 })
 export class PreloaderComponent implements OnInit {
-  loaded: boolean;
 
   constructor() {
   }
