2edbbd97e3c1c40a2e497f64e8c1087f35f511e5
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-13 21:28:38 +0400

    Apply dialog to profile photo

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,8 +2,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { MY_PROFILE_USERNAME } from '@app/core/variables/constants';
-import { SearchForm, User } from '@dt/interfaces/instagram';
+import { DialogData, SearchForm, User } from '@dt/interfaces/instagram';
 import { InstagramService } from '@app/core/services/instagram.service';
+import { MatDialog } from '@angular/material';
+import { PhotoDialogComponent } from '@app/photo-dialog/photo-dialog.component';
 
 @Component({
   selector: 'app-user',
@@ -21,7 +23,8 @@ export class UserComponent implements OnInit, OnDestroy {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private instaService: InstagramService
+    private instaService: InstagramService,
+    public dialog: MatDialog
   ) {
   }
 
@@ -46,6 +49,17 @@ export class UserComponent implements OnInit, OnDestroy {
 
   goToUser(username: string) {
     this.router.navigate(['user', username]);
+  }
+
+  onShowPhotoInDialog(photoUrl: string, caption: string = '', likes: number = 0) {
+    this.dialog.open(PhotoDialogComponent, {
+      maxWidth: 1000,
+      data: {
+        photoUrl: photoUrl,
+        caption: caption,
+        likes: likes
+      } as DialogData
+    });
   }
 
   goToExplore(tag: string) {
