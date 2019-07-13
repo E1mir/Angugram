cd8599efbbd13cae28b74ebba90b2088bd811f25
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-13 21:22:32 +0400

    Create and configure PhotoDialog component

---
+++ src/app/photo-dialog/photo-dialog.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { DialogData } from '@dt/interfaces/instagram';
+
+@Component({
+  selector: 'app-photo-dialog',
+  templateUrl: './photo-dialog.component.html',
+  styleUrls: ['./photo-dialog.component.scss']
+})
+export class PhotoDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<PhotoDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {
+  }
+
+  ngOnInit() {
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+}
