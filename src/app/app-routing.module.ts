c3da06f33c78e56460d8a2cb4a343ee34322ee18
Elmir Ismayilzade <kryternext@smartup.school>
2019-08-02 01:14:50 +0400

    Create Tag component & add routing for tag component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { HomeComponent } from '@app/home/home.component';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from '@app/core/components/page-not-found/page-not-found.component';
 import { UserComponent } from '@app/user/user.component';
+import { TagComponent } from './tag/tag.component';
 
 const appRoutes: Routes = [
   {
     path: '', component: HomeComponent, children: [
       {path: 'user', component: UserComponent},
-      {path: 'user/:username', component: UserComponent}
+      {path: 'user/:username', component: UserComponent},
+      {path: 'tag', component: TagComponent},
+      {path: 'tag/:tag', component: TagComponent}
     ]
   },
   {path: 'not-found', component: PageNotFoundComponent},
