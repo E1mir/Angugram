36825c5f0832e087e3528acded5021b53616182e
Elmir Ismayilzade <kryternext@smartup.school>
2019-08-02 21:16:58 +0400

    Create and apply tag posts component

---
+++ src/app/tag/tag-posts/tag-posts.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DialogData, Tag } from '@dt/interfaces/instagram';
+import { PhotoDialogComponent } from '@app/photo-dialog/photo-dialog.component';
+import { DIALOG_WIDTH } from '@app/core/variables/constants';
+import { MatDialog } from '@angular/material';
+
+@Component({
+  selector: 'app-tag-posts',
+  templateUrl: './tag-posts.component.html',
+  styleUrls: ['./tag-posts.component.scss']
+})
+export class TagPostsComponent implements OnInit {
+  @Input() tag: Tag = null;
+
+  constructor(
+    private dialog: MatDialog
+  ) {
+  }
+
+  onShowPhotoInDialog(postImageUrl: string, caption: string, likes: number, timestamp: number) {
+    this.dialog.open(PhotoDialogComponent, {
+      maxWidth: DIALOG_WIDTH,
+      data: {
+        photoUrl: postImageUrl,
+        caption: caption,
+        likes: likes,
+        timestamp: timestamp
+      } as DialogData
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+}
